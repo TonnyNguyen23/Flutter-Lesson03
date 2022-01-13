e0edcc517b1eeaf6f242efc210c2cdd175dfd5af
Tonny <nguyenxuantung473@gmail.com>
2022-01-13 15:39:55 +0700

    [1.0.0] Send & Receive Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-
-
+import 'package:overlay_support/overlay_support.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notify',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    return OverlaySupport(
+      child: MaterialApp(
+        title: 'Notify',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: MyHomePage(
+          title: '',
+        ),
       ),
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: '',),
     );
   }
 }
@@ -38,19 +40,60 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late int _totalNotifications;
   late final FirebaseMessaging _messaging;
+  PushNotification? _notificationInfo;
 
   @override
   void initState() {
     _totalNotifications = 0;
-    super.initState();
     registerNotification();
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      PushNotification notification = PushNotification(
+        title: message.notification?.title,
+        body: message.notification?.body,
+        dataTitle: message.data['title'],
+        dataBody: message.data['body'],
+      );
+      setState(() {
+        _notificationInfo = notification;
+        _totalNotifications++;
+      });
+    });
+
+    // Call here
+    checkForInitialMessage();
+
+    super.initState();
   }
+
+  Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    print("Handling a background message: ${message.messageId}");
+  }
+
+  checkForInitialMessage() async {
+    await Firebase.initializeApp();
+    RemoteMessage? initialMessage =
+    await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      PushNotification notification = PushNotification(
+        title: initialMessage.notification?.title,
+        body: initialMessage.notification?.body,
+      );
+      setState(() {
+        _notificationInfo = notification;
+        _totalNotifications++;
+      });
+    }
+  }
+
+
 
   void registerNotification() async {
     // 1. Initialize the Firebase app
     await Firebase.initializeApp();
 
-    FirebaseMessaging.instance.getToken().then((token)  {
+    FirebaseMessaging.instance.getToken().then((token) {
       print('FCM TOKEN:');
       print(token);
       print('END');
@@ -58,6 +101,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // 2. Instantiate Firebase Messaging
     _messaging = FirebaseMessaging.instance;
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
     // 3. On iOS, this helps to take the user permissions
     NotificationSettings settings = await _messaging.requestPermission(
@@ -70,12 +115,34 @@ class _MyHomePageState extends State<MyHomePage> {
     if (settings.authorizationStatus == AuthorizationStatus.authorized) {
       print('User granted permission');
       // TODO: handle the received notifications
+      // For handling the received notifications
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: Duration(seconds: 2),
+          );
+        }
+      });
     } else {
       print('User declined or has not accepted permission');
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +154,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             'App for capturing Firebase Push Notifications',
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -99,6 +166,28 @@ class _MyHomePageState extends State<MyHomePage> {
           NotificationBadge(totalNotifications: _totalNotifications),
           SizedBox(height: 16.0),
           // TODO: add the notification text here
+          _notificationInfo != null
+              ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'TITLE: ${_notificationInfo!.dataTitle ?? _notificationInfo!.title}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'BODY: ${_notificationInfo!.dataBody ?? _notificationInfo!.body}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          )
+              : Container(),
         ],
       ),
     );
@@ -131,4 +220,20 @@ class NotificationBadge extends StatelessWidget {
     );
   }
 }
+
+class PushNotification {
+  PushNotification({
+    this.title,
+    this.body,
+    this.dataTitle,
+    this.dataBody,
+
+  });
+  String? title;
+  String? body;
+  String? dataTitle;
+  String? dataBody;
+
+}
+
 
